7eb8e3b418d334229ce624b7f9a1721f7e8a8df7
andhry14 <andri.sopian14@yahoo.co.id>
2018-07-24 07:36:02 +0800

    Update Auto.js

--- Auto.js
+++ Auto.js
@@ -59,7 +59,7 @@ function showurl(datajson){
 			}
 			no++;
 		}
-		if (check == true) {
+		if (check == false) {
 			linktag[i].href = links[randindex] + setting.path + aesCrypto.encrypt(convertstr(linktag[i].href),convertstr('root'));
 			linktag[i].rel = "nofollow";
 			linktag[i].target = "_blank";
